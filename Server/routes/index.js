06a0c79e1af01391a4ae973b009c5981bbf6ba63
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-09-27 14:42:58 +0100

    server setup

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,3 +1,27 @@
-const express = require('express');
+import express from 'express';
+import http from 'http';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
 const app = express();
-const bodyParser = require('body-parser');
+require('dotenv').config();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false, }));
+
+app.use(bodyParser.json({ type: 'application/json', }))
+
+app.get('*', (req, res) =>
+  res.status(404).send({ message: 'Default 404 for everything', }));
+
+app.get('/', (req, res) =>
+  res.status(202).send({ message: 'Some info here', }));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+export default app;
