c638025b71fe46a464adbce31e3c50959293c894
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-10-02 18:21:45 +0100

    hound correction

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const fs = require('fs');
+
 const path = require('path');
-const Sequelize = require('sequelize');
+
+import Sequelize from 'sequelize';
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname} + /..\config\config.json`)[env];
