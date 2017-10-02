72d4c2e006e89607d2ba473899481e7e1e8ce11c
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-10-02 21:37:16 +0100

    users can now sign in

--- Server/controllers/handleUsers.js
+++ Server/controllers/handleUsers.js
@@ -27,3 +27,25 @@ exports.newUser = (req, res) => {
     });
   }
 }
+
+exports.userSignIn = (req, res) => {
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  if (userEmail && userPassword) {
+    users.create({ //if parameters were sent
+      email: userEmail,
+      password: userPassword,
+    })
+      .then((userCreated) => {
+        res.status(201).json({
+          status: 'Success',
+          message: 'You are now signed in'
+        });
+      }) //if unsuccessful
+      .catch(error => res.status(400).send(error));
+  } else {
+    response.status(400).json({
+      status: 'Unsuccessful', message: 'Missing data input'
+    });
+  }
+}
