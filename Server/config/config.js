01225de733fa4f2be1b375def0535b580d418883
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-10-05 20:45:05 +0100

    changes

--- Server/config/config.js
+++ Server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     database: 'recipes-dev',
     host: '127.0.0.1',
     port: '5433',
-    dialect: 'postgres',
+    dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USER,
@@ -20,7 +20,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: process.env.HEROKU_POSTGRESQL_COPPER_URL,
   },
 };
 
