0daf58812b518cd54c08a1101623ed8fbd782206
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-10-25 14:38:39 +0100

    set up react for signin components

--- client/components/signup/signinForm.js
+++ client/components/signup/signinForm.js
@@ -9,24 +9,32 @@ class signinForm extends React.Component {
     }
 
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value});
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
 
   render() {
     return (
       <div>
-        <form className="col s4 offset-s4">
+        <form onSubmit={this.onSubmit} className="col s4 offset-s4">
         <div className="input-field ">
           <label htmlFor="email"> Email: </label>
-          <input type="email" id="email" name='email' className="validate"/>
+          <input type="email" id="email" name='email' className="validate" value={this.state.email} onChange={this.onChange}/>
         </div>
         <div className="input-field ">
           <label htmlFor="pwd"> Password: </label>
-          <input type="password" id="pwd" className="validate"/>
+          <input type="password" id="pwd" name='password' className="validate" value={this.state.password} onChange={this.onChange}/>
         </div>
         <div className="right-align">
-          <button className="btn grey" type="button"> Sign In </button>
+          <input className="btn grey white-text" type="submit" value="Sign in"/>
         </div>
       </form>
     </div>
