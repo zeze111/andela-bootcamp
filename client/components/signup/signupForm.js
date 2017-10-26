b129b33b19f2b00d5530c0e706e94b4125561422
Osaze Edo-Osagie <ziggyzeze@yahoo.com>
2017-10-26 23:42:25 +0100

    textfieldgroup extraction

--- client/components/signup/signupForm.js
+++ client/components/signup/signupForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Validator from 'validatorjs';
 import userRules from '../../../Server/shared/validations';
+import TextFieldGroup from '../common/textFieldGroup';
 
 class signupForm extends React.Component {
   constructor(props) {
@@ -36,7 +37,7 @@ class signupForm extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    
+
 
     if (this.isValid()) {
       this.setState({ errors: {}, isLoading: true });
@@ -51,47 +52,57 @@ class signupForm extends React.Component {
 
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="col s5 offset-s4"> <br/>
-          <div className="input-field ">
-            <label htmlFor="fname"> First Name: </label>
-            <input type="text" id="fname" name='firstName' className="validate"
-              value={this.state.firstName} onChange={this.onChange} />
-            {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.firstName}</span>}
-          </div>
-          <div className="input-field ">
-            <label htmlFor="sname"> Surname: </label>
-            <input type="text" id="sname" name='surname' className="validate"
-              value={this.state.surname} onChange={this.onChange} />
-            {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.surname}</span>}
-          </div>
-          <div className="input-field ">
-            <label htmlFor="email"> Email: </label>
-            <input type="email" id="email" name='email' className="validate"
-              value={this.state.email} onChange={this.onChange} />
-            {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.email}</span>}
-          </div>
-          <div className="input-field ">
-            <label htmlFor="pwd"> Password: </label>
-            <input type="password" id="pwd" name='password' className="validate"
-              value={this.state.password} onChange={this.onChange} />
-            {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.password}</span>}
-          </div>
-          <div className="input-field ">
-            <label htmlFor="cf-pwd"> Password Confirmation: </label>
-            <input type="password" id="cf-pwd" name='password_confirmation' className="validate"
-              value={this.state.password_confirmation} onChange={this.onChange} />
-            {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.password_confirmation}</span>}
-          </div>
+        <form onSubmit={this.onSubmit} className="col s5 offset-s4"> <br />
+          <TextFieldGroup
+            label="First Name"
+            value={this.state.firstName}
+            onChange={this.onChange}
+            id="fname"
+            type="text"
+            name="firstName"
+            error={errors.firstName}
+          />
+          <TextFieldGroup
+            label="Surname"
+            value={this.state.surname}
+            onChange={this.onChange}
+            id="sname"
+            type="text"
+            name="surname"
+            error={errors.surname}
+          />
+          <TextFieldGroup
+            label="Email"
+            value={this.state.email}
+            onChange={this.onChange}
+            id="email"
+            type="email"
+            name="email"
+            error={errors.email}
+          />
+          <TextFieldGroup
+            label="Password"
+            value={this.state.password}
+            onChange={this.onChange}
+            id="pwd"
+            type="password"
+            name="password"
+            error={errors.password}
+          />
+          <TextFieldGroup
+            label="Password Confirmation"
+            value={this.state.password_confirmation}
+            onChange={this.onChange}
+            id="cf-pwd"
+            type="password"
+            name="password_confirmation"
+            error={errors.password_confirmation}
+          />
           <div className="right-align">
             <input disabled={this.state.isLoading} className="btn grey white-text" type="submit" value="Create" />
-          </div> <br/>
+          </div> <br />
           {errors && <span className='help-block' style={{ fontSize: 13 + 'px' }}>
-              {errors.message}</span>}
+            {errors.message}</span>}
         </form>
       </div>
     );
