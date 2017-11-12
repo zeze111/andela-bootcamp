c66d0aef8903d266338f9d7dfac770938304781d
Osaze <andeladeveloper@Osaze.local>
2017-11-12 11:51:25 +0100

    pagination for all recipes

--- Server/controllers/handleCrudRecipes.js
+++ Server/controllers/handleCrudRecipes.js
@@ -18,40 +18,39 @@ const handleCrudRecipe = {
     const validator = new Validator(req.body, validations.recipeRules);
     if (validator.passes()) {
       Recipe.findOne({
-        where: { 
+        where: {
           userId: req.decoded.id,
           name: req.body.name,
-        }
+        },
       })
         .then((userRecipe) => {
           if (userRecipe) {
             return res.status(400).json({
-                code: 400,
-                status: 'Unsuccessful',
-                message: 'Cannot Create A Recipe Twice',
-              });
-          } else {
-            Recipe.create({
-              name: req.body.name,
-              description: req.body.description,
-              prepTime: req.body.prepTime,
-              type: req.body.type,
-              ingredients: req.body.ingredients,
-              instructions: req.body.instructions,
-              userId: req.decoded.id,
-            })
-              .then((recipeCreated) => {
-                return res.status(201).json({
-                  code: 201,
-                  status: 'Success',
-                  recipeId: recipeCreated.dataValues.id,
-                  data: {
-                    recipe: `${recipeCreated.type}: ${recipeCreated.name} ${recipeCreated.prepTime}`,
-                  },
-                });
-              }) // if unsuccessful
-              .catch(error => res.status(400).send(error));
+              code: 400,
+              status: 'Unsuccessful',
+              message: 'Cannot Create A Recipe Twice',
+            });
           }
+          Recipe.create({
+            name: req.body.name,
+            description: req.body.description,
+            prepTime: req.body.prepTime,
+            type: req.body.type,
+            ingredients: req.body.ingredients,
+            instructions: req.body.instructions,
+            userId: req.decoded.id,
+          })
+            .then((recipeCreated) => {
+              return res.status(201).json({
+                code: 201,
+                status: 'Success',
+                recipeId: recipeCreated.dataValues.id,
+                data: {
+                  recipe: `${recipeCreated.type}: ${recipeCreated.name} ${recipeCreated.prepTime}`,
+                },
+              });
+            }) // if unsuccessful
+            .catch(error => res.status(400).send(error));
         })
         .catch(error => res.status(400).send(error));
     } else {
@@ -73,52 +72,76 @@ const handleCrudRecipe = {
     // returns a list of popular recipes, req has a '?' in it's header paramater
     if (req.query.sort) {
       Rating.findAll({
-				where: {
-					vote: 1
-				},
-				attributes: ['recipeId', [Sequelize.fn('count', Sequelize.col('vote')), 'Upvotes']],
-				include: [{
-					model: Recipe,
-					attributes: ['name', 'type', 'prepTime']
-				}],
-				order: [
-					['vote', 'DESC']
-				],
-				group: ['recipeId', 'vote', 'Recipe.id'],
-				limit: 5
-			})
-			.then((popularRecipes) => {
-				if (popularRecipes.length === 0) {
-					res.status(200).json({ // checks if list is empty
-						code: 200,
-						status: 'Successful',
-						message: 'There are no Popular Recipes',
-					});
-				} else {
-					res.status(200).json({
-						code: 200,
-						status: 'Successful',
-						data: popularRecipes,
-					});
-				}
-			})
-			.catch((error) => { res.status(400).send(error); });
-    } else {
-      Recipe.findAll({}).then((allRecipes) => {
-        if (allRecipes.length === 0) { // checks if the table is empty
-          res.status(200).json({
-            code: 200,
-            status: 'Successful',
-            message: 'Currently No Recipes',
-          });
-        } else {
-          res.status(200).json({
-            code: 200,
-            status: 'Successful',
-            data: allRecipes,
-          });
-        }
+        where: {
+          vote: 1,
+        },
+        attributes: ['recipeId', [Sequelize.fn('count', Sequelize.col('vote')), 'Upvotes']],
+        include: [{
+          model: Recipe,
+          attributes: ['name', 'type', 'prepTime'],
+        }],
+        order: [
+          ['vote', 'DESC'],
+        ],
+        group: ['recipeId', 'vote', 'Recipe.id'],
+        limit: 5,
       })
+        .then((popularRecipes) => {
+          if (popularRecipes.length === 0) {
+            res.status(200).json({ // checks if list is empty
+              code: 200,
+              status: 'Successful',
+              message: 'There are no Popular Recipes',
+            });
+          } else {
+            res.status(200).json({
+              code: 200,
+              status: 'Successful',
+              data: popularRecipes,
+            });
+          }
+        })
+        .catch((error) => { res.status(400).send(error); });
+    } else {
+      const limits = 3;   // number of records per page
+      let offsets = 0;
+      Recipe.findAndCountAll()
+        .then((data) => {
+          const page = parseInt(req.query.page, 10);      // page number
+          const pages = Math.ceil(data.count / limits);
+          offsets = limits * (page - 1);
+          Recipe.findAll({
+            attributes: ['name', 'description', 'prepTime', 'type'],
+            limit: limits,
+            offset: offsets,
+          }).then((allRecipes) => {
+            if (page > pages) {
+              res.status(404).json({
+                code: 404,
+                status: 'Unsuccessful',
+                message: 'Page Not Found',
+              });
+            }
+            if (allRecipes.length === 0) { // checks if the table is empty
+              res.status(200).json({
+                code: 200,
+                status: 'Successful',
+                message: 'Currently No Recipes',
+              });
+            } else {
+              res.status(200).json({
+                code: 200,
+                status: 'Successful',
+                data: allRecipes,
+                pageSize: limits,
+                totalCount: data.count,
+                currentPage: page,
+                pageCount: pages,
+              });
+            }
+          })
+            .catch(error => res.status(400).send(error));
+        })
         .catch(error => res.status(400).send(error));
     }
   },
@@ -128,7 +151,7 @@ const handleCrudRecipe = {
   * @param {Object} res - Response object
   * @returns {Object} Response object
   */
-  updateRecipe(req, res) { 
+  updateRecipe(req, res) {
     const recipeid = parseInt(req.params.recipeId, 10);
     Recipe.findById(recipeid)
       .then((recipe) => {
@@ -137,50 +160,50 @@ const handleCrudRecipe = {
             code: 404,
             status: 'Unsuccessful',
             message: 'Recipe Not Found',
-          }); //check if recipe belongs to user
-				} else if (recipe.userId === req.decoded.id) {
-					if (req.body.name || req.body.description || req.body.prepTime
-						|| req.body.type || req.body.ingredients || req.body.instructions) {
-						const validator = new Validator(req.body, validations.updateRecipeRules);
-						if (validator.passes()) {
-							recipe.update({
-								name: req.body.name || recipe.name,
-								description: req.body.description || recipe.description,
-								prepTime: req.body.prepTime || recipe.prepTime,
-								type: req.body.type || recipe.type,
-								ingredients: req.body.ingredients || recipe.ingredients,
-								instructions: req.body.instructions || recipe.instructions,
-							})
-								.then((updatedRecipe) => {
-									res.status(200).json({
-										code: 200,
-										status: 'Successful',
-										data: `${recipe.name} has been updated`,
-									});
-								})
-								.catch(error => res.status(400).send(error));
-						} else {
-							res.status(400).json({
-								code: 400,
-								status: 'Unsuccessful',
-								message: 'Invalid data input',
-								errors: validator.errors.all(),
-							});
-						}
-					} else {
-						res.status(400).json({
-							code: 400,
-							status: 'Unsuccessful',
-							message: 'Must input data',
-						});
-					}
-				} else {
-					res.status(401).json({
-						code: 401,
-						status: 'Unsuccessful',
-						message: 'You are Unauthorized',
-					});
-				}
+          }); // check if recipe belongs to user
+        } else if (recipe.userId === req.decoded.id) {
+          if (req.body.name || req.body.description || req.body.prepTime
+      || req.body.type || req.body.ingredients || req.body.instructions) {
+            const validator = new Validator(req.body, validations.updateRecipeRules);
+            if (validator.passes()) {
+              recipe.update({
+                name: req.body.name || recipe.name,
+                description: req.body.description || recipe.description,
+                prepTime: req.body.prepTime || recipe.prepTime,
+                type: req.body.type || recipe.type,
+                ingredients: req.body.ingredients || recipe.ingredients,
+                instructions: req.body.instructions || recipe.instructions,
+              })
+                .then((updatedRecipe) => {
+                  res.status(200).json({
+                    code: 200,
+                    status: 'Successful',
+                    data: `${recipe.name} has been updated`,
+                  });
+                })
+                .catch(error => res.status(400).send(error));
+            } else {
+              res.status(400).json({
+                code: 400,
+                status: 'Unsuccessful',
+                message: 'Invalid data input',
+                errors: validator.errors.all(),
+              });
+            }
+          } else {
+            res.status(400).json({
+              code: 400,
+              status: 'Unsuccessful',
+              message: 'Must input data',
+            });
+          }
+        } else {
+          res.status(401).json({
+            code: 401,
+            status: 'Unsuccessful',
+            message: 'You are Unauthorized',
+          });
+        }
       })
       .catch(error => res.status(400).send(error));
   },
@@ -190,7 +213,7 @@ const handleCrudRecipe = {
   * @param {Object} res - Response object
   * @returns {Object} Response object
   */
-  deleteRecipe(req, res) { 
+  deleteRecipe(req, res) {
     const recipeid = parseInt(req.params.recipeId, 10);
     Recipe.findById(recipeid)
       .then((recipe) => {
@@ -211,12 +234,12 @@ const handleCrudRecipe = {
             })
             .catch(error => res.status(400).send(error));
         } else {
-					res.status(401).json({
-						code: 401,
-						status: 'Unsuccessful',
-						message: 'You are Unauthorized',
-					});
-				}
+          res.status(401).json({
+            code: 401,
+            status: 'Unsuccessful',
+            message: 'You are Unauthorized',
+          });
+        }
       })
       .catch(error => res.status(400).send(error));
   },
