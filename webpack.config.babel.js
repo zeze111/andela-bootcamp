b136744bca5ac4cb905c074c68eb13dada2817db
Osaze <andeladeveloper@Osaze.local>
2018-01-17 22:04:38 +0100

    heroku based correction

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -17,6 +17,11 @@ export default {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
   ],
   module: {
     loaders: [
