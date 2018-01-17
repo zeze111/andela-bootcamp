e717bf6a3a8e9d46a197ef6c16aa6cfdacbbc9ce
Osaze <andeladeveloper@Osaze.local>
2018-01-17 21:53:02 +0100

    heroku based corrections

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -38,6 +38,29 @@ export default {
           loader: 'sass-loader', // compiles Sass to CSS
         }],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]', {
+            loader: 'image-webpack-loader',
+            query: {
+              mozjpeg: {
+                progressive: true,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              pngquant: {
+                quality: '75-90',
+                speed: 3,
+              },
+            },
+          }],
+        exclude: /node_modules/,
+      },
     ],
   },
   resolve: {
