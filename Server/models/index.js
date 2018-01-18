d3c6ae0de79ce9924c6f7d0ac5fea15fd20de75c
Osaze <andeladeveloper@Osaze.local>
2018-01-18 10:37:36 +0100

    heroku based correction

--- Server/models/index.js
+++ Server/models/index.js
@@ -9,7 +9,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config`)[env];
-const db = {};
+global.db = {};
 
 let sequelize;
 // From the environment, extract the key with the name provided
