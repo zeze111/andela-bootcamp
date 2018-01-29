58318733978f1c10151141bfca05fdb1153863d3
Osaze <andeladeveloper@Osaze.local>
2018-01-29 11:42:54 +0100

    debugg HOC

--- client/utils/decodeToken.js
+++ client/utils/decodeToken.js
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 const decodeToken = () => {
-  const userToken = localStorage.getItem('jwToken');
+  const userToken = localStorage.getItem('jwtToken');
   if (userToken) {
     return jwt.verify(userToken, process.env.SECRET_KEY, ((error) => {
+      console.log('error 1', error);
       if (!error) {
+        console.log('some value');
         return true;
       }
-      localStorage.removeItem('jwToken');
+      console.log('error', error);
+      localStorage.removeItem('jwtToken');
       return false;
     }));
   }
-  return false;
+  console.log(false);
 };
 
 export default decodeToken;
